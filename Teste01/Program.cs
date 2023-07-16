9241e5cce660f91f0165cf300b6fe77153a9730c
FabianoQurino <fabianoquirino2011@hotmail.com>
2023-07-16 19:32:08 -0300

    Primeiro teste feito com sucesso!!

---
+++ Teste01/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Teste01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a nota do primeiro semestre:");
+            float notaPrimeiroSemestre = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a nota do segundo semestre:");
+            float notaSegundoSemestre = float.Parse(Console.ReadLine());
+            float media = (notaPrimeiroSemestre + notaSegundoSemestre) / 2;
+            Console.WriteLine("Sua média é de " + media);
+        }
+    }
+}
